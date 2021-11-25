707e82b8d26a0becbdcc1ca2ad93d470f7267d14
Szalai Szabolcs <sz.szalai16@gmail.com>
2021-11-25 12:17:25 +0100

    done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,86 @@
 package sample;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Controller {
+    @FXML
+    private Button btnStartStop;
+    @FXML
+    private Button btnResetReszido;
+    @FXML
+    private Label stopper;
+    @FXML
+    private ListView<String> reszidoLista;
+    private boolean running;
+    private Timer stopperTimer;
+    private LocalDateTime startTime;
+
+    @FXML
+    public void initialize() {
+        running = false;
+    }
+
+    @FXML
+    public void startStopClick() {
+        if (running) {
+            stopperStop();
+        } else {
+            stopperStart();
+        }
+    }
+
+    private void stopperStart() {
+        btnStartStop.setText("Stop");
+        btnResetReszido.setText("Részidő");
+        running = true;
+        stopperTimer = new Timer();
+        startTime = LocalDateTime.now();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Duration elteltIdo = Duration.between(startTime, LocalDateTime.now());
+                int perc = elteltIdo.toMinutesPart();
+                int masodperc = elteltIdo.toSecondsPart();
+                int ms = elteltIdo.toMillisPart();
+                Platform.runLater(() -> stopper.setText(String.format("%02d:%02d:%03d", perc, masodperc, ms)));
+            }
+        };
+        stopperTimer.schedule(timerTask,1,1);
+    }
+
+    private void stopperStop() {
+        btnStartStop.setText("Start");
+        btnResetReszido.setText("Reset");
+        running = false;
+        stopperTimer.cancel();
+    }
+
+    @FXML
+    public void resetReszidoClick() {
+        if (running) {
+            reszido();
+        } else {
+            reset();
+        }
+    }
+
+    private void reset() {
+        stopper.setText("00:00.000");
+        reszidoLista.getItems().clear();
+    }
+
+    private void reszido() {
+        String reszido = stopper.getText();
+        reszidoLista.getItems().add(reszido);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,8 +12,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Stopper");
         primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setOnCloseRequest(event -> System.exit(0));
         primaryStage.show();
     }
 
